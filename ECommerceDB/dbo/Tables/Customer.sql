8bf65607d4acec8bfa9e37acaa5ff095a8f9f31c
Vidushi Singhvi <vidushi.singhvi@dnow.com>
2025-02-27 18:45:25 +0530

    Customer Table Added

---
+++ ECommerceDB/dbo/Tables/Customer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	customer_id int primary key,
+	FirstName varchar(50),
+	MiddleName varchar(50),
+	LastName varchar(50),
+	Email varchar(100),
+	DateOfBirth date,
+	phone int,
+	age int null
+)
